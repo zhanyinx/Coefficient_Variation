4f8e214f0f96adc56983db2f03af9efce3f5bf2b
zhanyinx <yinxiu.zhan@fmi.ch>
2016-12-08 12:18:16 +0100

    Uploading

---
+++ convert.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+bin=5000
+s=99000000
+e=103350000
+awk 'BEGIN{getline;getline; min=9999999999999;max=-99; bin=0.+"'"$bin"'"; s=0.+"'"$s"'"; e=0.+"'"$e"'"}{
+	split($1,a,"-"); 
+	split(a[1],b,":");
+	start=b[2]
+
+        split($2,c,"-"); 
+        split(c[1],d,":");
+        end=d[2]
+
+	matrix[start,end]=$3
+	bool[start,end]++
+	if(start>max) max=start
+	if(start<min) min=start
+		
+}END{
+	for(i=min;i<=max;i+=bin) if(i>s && i<e) printf "%s ", "chrX:"i
+	printf "\n"
+	for(i=min;i<=max;i+=bin){
+		if(i>s && i<e) printf "%s ","chrX:"i
+		for(j=min;j<=max;j+=bin){
+			if(i>s && i<e && j>s && j<e){ 
+				if(bool[i,j]>0) printf "%s ", matrix[i,j];
+				else printf "%s ", "0"  
+			}
+		}
+		if(i>s && i<e)	printf "\n"
+	}
+
+}' $1 > $1.matrix
+
+
